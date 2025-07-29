294b8d31bd3ebf7ced8bc869bd4a5df6c228d731
Kevin Anderson Santos Lopez <kevin.santos@grupoemco.com>
2025-07-29 10:29:48 -0600

    changes

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,15 @@
+import os
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
 
-SQLACHEMY_DATABASE_URL = "mysql+pymysql://root:Conexion+2025@localhost/marketplacedb"
+# Cargar variables del .env
+load_dotenv()
+
+SQLACHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not SQLACHEMY_DATABASE_URL:
+    raise ValueError("DATABASE_URL no está definida en el archivo .env")
 
 engine = create_engine(SQLACHEMY_DATABASE_URL, echo=True)
 
